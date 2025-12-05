ef3429d2b5b26f9f74b331fcd77d99a485bdd1d5
Stephanie Callec <stephanie.callec@sekoia.io>
2025-12-05 13:15:26 +0100

    fix: Remove duplicate print statement in Workday module initialization

--- modules/TestWorkday/main.py
+++ modules/TestWorkday/main.py
@@ -2,6 +2,7 @@ from workday import WorkdayModule
 from workday.workday_activity_logging_connector import WorkdayActivityLoggingConnector
 
 if __name__ == "__main__":
+    print("Starting Workday module...")
     module = WorkdayModule()
     module.register(WorkdayActivityLoggingConnector, "workday_activity_logging")
     module.run()
